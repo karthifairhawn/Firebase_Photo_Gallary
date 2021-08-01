ce3039f0cbaba9200ad2c3614433be8b144409ce
Karthi Fair Hawn <karthifairhawn@gmail.com>
2021-08-01 21:47:06 +0530

    Progress Bar

--- src/comps/progressBar.js
+++ src/comps/progressBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import useStorage from '../hooks/useStorage';
+import { useEffect } from 'react';
 
 const ProgresBar = ( {file, setFile }) => {
     const { url, progress } = useStorage(file);
     console.log(progress,url);
+    useEffect(() => {
+        if(url){
+            setFile(null);
+        }
+    }, [url]);
     return ( 
-        <div className="progress-bar">
-            progress
+        <div className="progress-bar" style={ {width: progress+'%'}}>
+            
         </div>
      );
 }
